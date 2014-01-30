edee81b55d7dc79fd7f03118e581433d42f18392
Angelio <angelino.capretti@gmail.com>
2014-01-30 17:24:48 -0500

    update fibonacci

---
+++ ex4/ex04_01.dart
@@ -0,0 +1,26 @@
+main (){
+int a = 0;
+int b = 1;
+var c = a + b;
+var d = b + c;
+var e = c + d;
+var f = d + e;
+var g = e + f;
+var h = f + g;
+var i = g + h;
+var j = h + i;
+var k = i + j;
+
+print ('''Fibonnacci 
+    $a
+    $b
+    $c
+    $d
+    $e
+    $f
+    $g
+    $h
+    $i
+    $j
+    $k ''');
+}
