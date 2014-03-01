0c1ff63b2bf092ec277248320b54295d2f830fb0
Angelio <angelino.capretti@gmail.com>
2014-02-28 20:33:37 -0500

    check

--- AC_ondartme_exercices/ex2/ex02_4.dart
+++
@@ -1,8 +0,0 @@
-main () {
-  var distance = 100;
-  var temps = 9.58;
-  var MS = distance / temps;
-  var KMH = MS * 3.6;
-  print ('Solution 4) = $KMH');
-    
-}
--- AC_ondartme_exercices/ex3/ex3_bis.dart
+++
@@ -1,8 +0,0 @@
-void main (){
-
-  for (var i=1; i<=100; i++)
-    print(i % 15 == 0 ? "$i : FizzBuzz" :
-      i % 3 == 0 ? "$i : Fizz" : 
-        i % 5 == 0 ? "$i : Buzz" :
-          i);
-}
